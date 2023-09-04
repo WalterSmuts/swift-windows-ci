6d3ba092ce50812d433f72b2b6c76ed4da5c95a9
Walter Smuts <smuts.walter@gmail.com>
2023-09-04 10:44:46 +0200

    Add test

--- Package.swift
+++ Package.swift
@@ -6,10 +6,8 @@ import PackageDescription
 let package = Package(
     name: "swift-windows-ci",
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "swift-windows-ci",
-            path: "Sources"),
+    .testTarget(
+      name: "dummy",
+      swiftSettings: []),
     ]
 )
